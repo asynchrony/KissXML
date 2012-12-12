f636eabb5b6065fde3fce34281b2a0bfd09b54b7
Nate Minshew <nate.minshew@asolutions.com>
2012-12-12 11:32:20 -0600

    Added framework project.

---
+++ KissXMLFramework/KissXMLFramework/KissXMLFramework.h
@@ -0,0 +1,13 @@
+//
+//  KissXMLFramework.h
+//  KissXMLFramework
+//
+//  Created by Nate Minshew on 12/12/12.
+//  Copyright (c) 2012 Asynchrony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KissXMLFramework : NSObject
+
+@end
---
+++ KissXMLFramework/KissXMLFramework/KissXMLFramework.m
@@ -0,0 +1,13 @@
+//
+//  KissXMLFramework.m
+//  KissXMLFramework
+//
+//  Created by Nate Minshew on 12/12/12.
+//  Copyright (c) 2012 Asynchrony. All rights reserved.
+//
+
+#import "KissXMLFramework.h"
+
+@implementation KissXMLFramework
+
+@end
